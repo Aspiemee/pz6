a9b0844ab5544a6516b433127faa0ad9a8d7b028
Aspiemee <aspiesteam@mail.ru>
2024-04-13 21:46:20 +0300

    была добавленна функция рисования шестиугольника

--- pz6.py
+++ pz6.py
@@ -11,6 +11,7 @@ args = parser.parse_args()
 name = args.name
 height = args.height
 
+
 def triangle(h: int) -> None:
     ...
 
@@ -21,7 +22,12 @@ def trapezoid(h: int) -> None:
     ...
 
 def hexagon(h: int) -> None:
-    ...
+    if h < 5:
+        print('Высота шестиугольника не может быть меньше 5!')
+    else:
+        print(' ' + '*' * 7)
+        for i in range(h - 2):
+            print('*'*9)
     
 match name.lower():
     case 'квадрат':
